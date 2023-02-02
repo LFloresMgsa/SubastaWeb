1ab1b6151f90a4b4d0420733eac3b6d305ea7af2
miguel-ls <miguel.lopez@codesicorp.com>
2023-02-02 13:53:26 -0500

    comit

--- src/views/Catalogo.js
+++ src/views/Catalogo.js
@@ -1,10 +1,8 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 import CompShowBlogs from '../blog/ShowBlog';
 
 const Catalogo = () => {
 
-  const history = useHistory();
 
   return (
     <div >
