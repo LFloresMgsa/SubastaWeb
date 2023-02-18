6eb5beebe826a03f15318b57a22bb2a9c9c840fc
miguel-ls <miguel.lopez@codesicorp.com>
2023-02-18 12:36:15 -0500

    catalogo

--- src/blog/ShowBlog.js
+++ src/blog/ShowBlog.js
@@ -4,80 +4,89 @@ import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 import { useHistory } from 'react-router-dom';
 
-const URI = 'http://localhost:8000/blogs/'
+const URI = 'http://localhost:5000/api/catalogo'
 
 const CompShowBlogs = (props) => {
 
-    const history = useHistory();    
-    const [blogs, setBlog] = useState([])
-    useEffect(() => {
-        getBlogs()
-    }, [])
+  const history = useHistory();
+  const [catalogos, setCatalogo] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getCatalogos();
+  }, []);
 
 
-    const handleCrear = () => {
-        history.push({
-            pathname: '/Create',
-            state: {props }
-        });
-    }    
-
-    const handleEditar = (id) => {
-        history.push({
-            pathname: `/Update/${id}`,
-            state: {props }
-        });
-    }  
-
-    // procedimiento para mostrar todos los blogs
-    const getBlogs = async () => {
-        const res = await axios.get(URI)
-        setBlog(res.data)
-
-    }
-
-    // procedimiento para eliminar un blog
-    const deleteBlog = async (id) => {
-        await axios.delete(`${URI}${id}`)
-        getBlogs()
-    }
-
-    return (
-        <div className='container'>
-        <div className='row'>
-          <div className='col'>
-            
-            <Button variant="contained" size="small" color="primary" onClick={handleCrear}>Crear</Button>
+  /*     const handleCrear = () => {
+          history.push({
+              pathname: '/Create',
+              state: {props }
+          });
+      }    
   
-            <table className='table'>
-              <thead className='table-primary'>
-                <tr>
-                <th>Id</th>
-                  <th>Title</th>
-                  <th>Content</th>
-                  <th>Actions</th>
+      const handleEditar = (id) => {
+          history.push({
+              pathname: `/Update/${id}`,
+              state: {props }
+          });
+      }   */
+
+  // procedimiento para mostrar todos los blogs
+  const getCatalogos = async () => {
+    try {
+      const response = await axios.get(URI);
+      setCatalogo(response.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  /*     // procedimiento para eliminar un blog
+      const deleteBlog = async (id) => {
+          await axios.delete(`${URI}${id}`)
+          getBlogs()
+      } */
+
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+
+          {/*  <Button variant="contained" size="small" color="primary" onClick={handleCrear}>Crear</Button> */}
+
+          <table className='table'>
+            <thead className='table-primary'>
+              <tr>
+                <th>Cab_cCatalogo</th>
+                <th>Emp_cCodigo</th>
+
+                <th>Cab_cDescripcion</th>
+                <th>Propietario</th>
+              </tr>
+            </thead>
+            <tbody>
+              {catalogos.map((catalogo) => (
+                <tr key={catalogo.Cab_cCatalogo}>
+                  <td> {catalogo.Emp_cCodigo} </td>
+                  <td> {catalogo.Cab_cDescripcion} </td>
+                  <td> {catalogo.Propietario} </td>
+                  <td>
+
+                    {/*                       <Link to={`/Update/${blog.Cab_cCatalogo}`} >Editar</Link>
+                      <button onClick={() => deleteBlog(blog.Cab_cCatalogo)} >Eliminar</button> */}
+
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {blogs.map((blog) => (
-                  <tr key={blog.id}>
-                    <td> {blog.id} </td>
-                    <td> {blog.title} </td>
-                    <td> {blog.content} </td>
-                    <td>
-                     {/* <Button variant="contained" size="small" color="primary" onClick={handleEditar(blog.id)}>Editar</Button>  */}
-                      <Link to={`/Update/${blog.id}`} >Editar</Link>
-                      <button onClick={() => deleteBlog(blog.id)} >Eliminar</button>
-                      
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default CompShowBlogs
