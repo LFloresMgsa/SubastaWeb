f1277767d434fc3c3a43563108abf07157b3c1de
miguel-ls <miguel.lopez@codesicorp.com>
2023-05-07 01:21:16 -0500

    .

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,7 +1,12 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect} from 'react';
 
 import { css, useTheme } from 'styled-components';
 import { styled } from '@mui/material/styles';
+
+
+
+import Slider from './SliderBackground.js'
+import './SliderBackground.css'
 
 const StyledView = styled('div')(
   ({ theme }) => css`
@@ -9,12 +14,49 @@ const StyledView = styled('div')(
   `
 );
 
-const Dashboard = (props) => {
-  return (
-    <div>
-      <h1>Bienvenidos a Galpon Legado</h1>
-    </div>
-  );
+const  Dashboard = () => {
+
+
+  const images = [
+    {
+      id: '1',
+      title: 'Awesome forest',
+      image:
+        'https://cdn.pixabay.com/photo/2020/11/09/15/12/trail-5726987_960_720.jpg',
+    },
+    {
+      id: '2',
+      title: 'A litle bird.',
+      image:
+        'https://cdn.pixabay.com/photo/2011/09/27/18/52/bird-9950_960_720.jpg',
+    },
+    {
+      id: '3',
+      title: 'The best friend.',
+      image:
+        'https://cdn.pixabay.com/photo/2017/12/27/14/02/friends-3042751_960_720.jpg',
+    },
+    {
+      id: '4',
+      title: 'Beautiful desert.',
+      image:
+        'https://cdn.pixabay.com/photo/2016/11/21/17/44/arches-national-park-1846759_960_720.jpg',
+    },
+    {
+      id: '5',
+      title: 'Harley Davidson motorcycle.',
+      image:
+        'https://cdn.pixabay.com/photo/2018/10/26/22/55/harley-davidson-3775527_960_720.jpg',
+    },
+  ]
+  
+
+    return (
+      <div className='App'>
+        <Slider images={images} />
+      </div>
+    );
+
 };
 
 export default Dashboard;
