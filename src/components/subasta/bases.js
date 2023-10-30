8bdfd5c6070999db9c0467feb04923877f85816f
MANAGEMENT GROUP SA <mlopez@mgsa.com.pe>
2023-10-30 16:18:08 -0500

    subasta octubre

--- src/components/subasta/bases.js
+++ src/components/subasta/bases.js
@@ -60,7 +60,7 @@ const Bases = () => {
 
 
                 <div>
-                    <ListItem>La subasta culminará el día miercoles 30 de agosto a las 9:00 pm, momento en el que se determinarán a los ganadores de los ejemplares subastados.</ListItem>
+                    {/* <ListItem>La subasta culminará el día miercoles 30 de agosto a las 9:00 pm, momento en el que se determinarán a los ganadores de los ejemplares subastados.</ListItem>
                     <ListItem>Cada puja se realizará por múltiplos de S/50.00 (cincuenta soles) sobre el precio base de cada ejemplar.</ListItem>
                     <ListItem>De existir 2 (dos) o más pujas por algún ejemplar a falta de 10 (diez) minutos para el cierre de la subasta, se aumentarán 10 (diez) minutos adicionales, generando una nueva hora de cierre, y así sucesivamente, hasta definir a un ganador. La hora límite para el cierre será indefectiblemente a las 00:00 horas de jueves 31 de agosto.</ListItem>
                     <ListItem>Se eliminarán las pujas de quienes coloquen DNI y/o celular falsos o de otra persona. Toda puja se verificará por vía telefónica, por transparencia.</ListItem>
@@ -73,6 +73,21 @@ const Bases = () => {
 
                     <ListItem>Enviar el comprobante de pago al celular 950793210. De contar con otro medio de pago, se deberá coordinar previamente.</ListItem>
                     <ListItem>El ganador se encargará de todos los gastos de envío del ejemplar adjudicado y el plazo máximo para el recojo de las aves es de 7 (siete) días. No se entregarán animales sin la previa comprobación del pago e identidad de los ganadores.</ListItem>
+                    <ListItem>Cualquier controversia que surgiera en la presente subasta será resuelta por la  organización.</ListItem> */}
+
+                    <ListItem>La subasta iniciará el día lunes 30 de octubre al mediodía y culminará el día miércoles 1 de noviembre a las 9:00 pm, momento en el que se determinarán a los ganadores de los ejemplares subastados.</ListItem>
+                    <ListItem>Cada puja se realizará por múltiplos de S/50.00 (cincuenta soles) sobre el precio base de cada ejemplar.</ListItem>
+                    <ListItem>De existir 2 (dos) o más pujas por algún ejemplar a falta de 10 (diez) minutos para el cierre de la subasta, se aumentarán 10 (diez) minutos adicionales, generando una nueva hora de cierre, y así sucesivamente, hasta definir a un ganador. La hora límite para el cierre será indefectiblemente a las 00:00 horas del jueves 2 de noviembre.</ListItem>
+                    <ListItem>Se eliminarán las pujas de quienes coloquen DNI y/o celular falsos o de otra persona. Toda puja se verificará por vía telefónica, por transparencia.</ListItem>
+                    <ListItem>Nos comunicaremos, previo abono, con los ganadores para coordinar la recepción o envío de los ejemplares. El pago deberá realizarse dentro de las 24 (veinticuatro) horas posteriores a la finalización de la subasta.</ListItem>
+                    <ListItem>Si el ganador de algún ejemplar no respondiera o no realizara el pago en el plazo indicado, se dará por nuevo ganador a quien realizó la siguiente puja más alta y así sucesivamente.</ListItem>
+                    <ListItem>Los pagos serán realizados a la siguiente cuenta a nombre de María Isabel Pascual:</ListItem>
+
+                    <ListItemBullet className={classes.bullet}>- BCP soles: 19414466443067</ListItemBullet>
+                    <ListItemBullet className={classes.bullet}>- CCI: 00219411446644306790</ListItemBullet>
+
+                    <ListItem>Enviar el comprobante de pago al celular 996504380. De contar con otro medio de pago, se deberá coordinar previamente.</ListItem>
+                    <ListItem>El ganador se encargará de todos los gastos de envío del ejemplar adjudicado y el plazo máximo para el recojo de las aves es de 7 (siete) días. No se entregarán animales sin la previa comprobación del pago e identidad de los ganadores.</ListItem>
                     <ListItem>Cualquier controversia que surgiera en la presente subasta será resuelta por la  organización.</ListItem>
                 </div>
 
@@ -84,3 +99,7 @@ const Bases = () => {
 };
 
 export default Bases; 1
+
+//----------------- comando para compilar proyecto react -----------------
+// npm run guild
+//------------------------------------------------------------------------
